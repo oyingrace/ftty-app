dc23a5267d832a2296ee0f4b86129f749a9ffc0a
oyingrace <oyingrace307@gmail.com>
2025-11-20 20:50:33 +0100

    integrate AppKitProvider in RootLayout for enhanced application context management

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 // app/layout.js
 import './globals.css';
+import { AppKitProvider } from '@reown/appkit/react'
 
 export const metadata = {
   title: 'FTTY - Gaming Assets Crypto Marketplace',
@@ -9,9 +10,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <AppKitProvider
+      projectId="5acef91d7967e48a748a4a68f1cb790c"
+      networks={[
+      ]}
+    >
       <body>
         {children}
       </body>
+      </AppKitProvider>
     </html>
   );
 }
